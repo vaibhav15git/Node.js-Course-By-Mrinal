32eb19d5425d4af101a163577a2c53fd6a105461
Vaibhav <103978586+vaibhav15git@users.noreply.github.com>
2023-04-13 12:45:08 +0530

    ok

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-console.log("Hello Node")
+// console.log("Hello Node")
 
-function sayHello(){
-    console.log("Hello")
-}
-sayHello()
+// function sayHello(){
+//     console.log("Hello")
+// }
+// sayHello()
+
+let name = "Raj";
+console.log(global);
