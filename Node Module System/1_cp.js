262f647dd676d38fe2a7ccaae4bb825ac984a4c7
Vaibhav <103978586+vaibhav15git@users.noreply.github.com>
2023-04-13 23:57:32 +0530

    os module done

---
+++ Node Module System/1_cp.js
@@ -0,0 +1,8 @@
+
+const cp = require('child_process');
+
+// cp.execSync('calc');
+
+// cp.execSync('start chrome https://www.scaler.com/topics/');
+
+console.log('output'+ cp.execSync('node demo.js'));
---
+++ Node Module System/demo.js
@@ -0,0 +1,1 @@
+console.log('hello node I am demo.js');
