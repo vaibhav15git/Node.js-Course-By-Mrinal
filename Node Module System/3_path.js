b716435bda9ae406e34beb63d37d81f68c99e7db
Vaibhav <103978586+vaibhav15git@users.noreply.github.com>
2023-04-15 23:03:42 +0530

    demo

---
+++ Node Module System/3_path.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+let ext = path.extname(
+  "C:Users\vaibhDesktopNode.js-Course-By-MrinalNode Module System\f1.txt"
+);
+
+let baseName = path.basename(
+  "C:Users\\vaibhDesktopNode.js-Course-By-MrinalNode Module System\\f1.txt"
+);
+
+console.log(ext);
+
+console.log(baseName);
+console.log(__filename);
+console.log(__dirname);
